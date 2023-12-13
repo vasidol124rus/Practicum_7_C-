5bcc1f0f31db0f144ddd858ed1f35f762d4d2947
Василий Кузнецов <vasekku25@gmail.com>
2023-12-13 17:17:10 +0700

    задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 1, 2, 3, 4, 5 };
+void PrintArrayNambers(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(array[index] + " ");
+            PrintArrayNambers(array, index - 1);
+        }
+    }
+Console.Clear();
+PrintArrayNambers(array, array.Length - 1);
+
+
