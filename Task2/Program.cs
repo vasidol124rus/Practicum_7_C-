132c72052f5698d72ae3ce059dfc9fb21f6aa84c
Василий Кузнецов <vasekku25@gmail.com>
2023-12-13 15:04:36 +0700

    Задача2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int akkerman(int m, int n)
+{
+if (m == 0) 
+    return n + 1;
+else 
+    if (n == 0) 
+        return akkerman(m - 1, 1);
+    else 
+        return akkerman(m - 1, akkerman(m, n - 1));
+}
+Console.Clear();
+Console.Write("Введите неотрицательное число m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите неотрицательное число n: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write($"Функция Аккермана равно {akkerman(m, n)} ");
